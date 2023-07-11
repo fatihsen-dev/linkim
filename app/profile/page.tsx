280ec1e954a684b1e8fc9869729aff0de1d9b852
Fatih Şen <fatihydrm@outlook.com>
2023-07-11 13:15:24 +0300

    edit: design profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,20 @@
-export default function page() {
-   return <div>Profile</div>;
+"use client";
+import Avvvatars from "avvvatars-react";
+import { useAuthStore } from "@/store/auth";
+
+export default function Page() {
+   const { user } = useAuthStore();
+
+   return (
+      <div className="flex h-full gap-10 pb-10">
+         <div className="h-full p-10 max-w-[300px] w-full flex flex-col rounded-md border border-white/10 items-center gap-5 bg-white/5 backdrop-blur-md">
+            <Avvvatars size={70} style="shape" value={user?.email || ""} />
+            <div className="text-center">
+               <h3 className="text-xl leading-4 font-semibold text-white">{user?.user_metadata.displayName}</h3>
+               <span className="leading-4 text-white/50">{user?.user_metadata.username}</span>
+            </div>
+         </div>
+         <div className="h-full px-10 py-5 flex-1 rounded-md border border-white/10 bg-white/5 backdrop-blur-md"></div>
+      </div>
+   );
 }
