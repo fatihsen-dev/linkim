bcbf21d79715af0499f6c198576ceed273d44be0
Fatih Şen <fatihydrm@outlook.com>
2023-07-14 11:15:07 +0300

    edit: button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,17 @@
 interface PropsT {
    children: React.ReactNode;
    className?: string;
+   type?: "button" | "reset" | "submit";
+   disabled?: boolean;
    onClick?: () => void;
 }
 
-export default function Button({ className, onClick, children }: PropsT) {
+export default function Button({ className, onClick, children, type }: PropsT) {
    return (
       <button
+         type={type}
          onClick={onClick}
-         className={`bg-green-500 hover:bg-opacity-80 transition-colors px-10 py-3 rounded-full text-white ${className}`}
+         className={`bg-green-500 hover:bg-opacity-80 transition-colors px-10 h-10 rounded-full text-white ${className}`}
       >
          {children}
       </button>
