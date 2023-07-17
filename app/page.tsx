8ca9d800dcf14467727e0e4c4e3aa3d20e25e7f6
Fatih Şen <fatihydrm@outlook.com>
2023-07-17 22:20:34 +0300

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
    return (
       <div className="flex h-full 2xl:flex-row lg:flex-row">
          <div className="2xl:flex-1 sm:flex-1 py-5 justify-center 2xl:items-start sm:items-start items-center flex-col 2xl:gap-10 lg:gap-10 gap-5 2xl:flex lg:flex hidden">
-            <p className="text-white font-light leading-6 2xl:text-xl md:text-xl text-lg 2xl:inline-block sm:inline-block hidden">
+            <p className="text-white font-extralight leading-6 2xl:text-xl md:text-xl text-lg 2xl:inline-block sm:inline-block hidden">
                <span className="font-bold text-green-500">Linkim</span>
                {`, İnternet'in hızına uyum sağlamak ve linklerinizi daha kolay paylaşılabilir hale getirmek için
                tasarlanmış yenilikçi bir link kısaltma web projesidir. Artık uzun ve karmaşık URL'leri paylaşmak zorunda
@@ -35,12 +35,12 @@ export default function Home() {
                   disabled={user ? true : false}
                   value={username}
                   onInput={(e: any) => setUsername(e.target.value)}
-                  className="outline-none flex-1 max-w-[150px]"
+                  className="outline-none flex-1 max-w-[140px]"
                   type="text"
                />
             </label>
             <div
-               style={{ pointerEvents: user ? "none" : "auto", opacity: user ? 0.8 : 1 }}
+               style={{ pointerEvents: user ? "none" : "auto", opacity: user ? 0.7 : 1 }}
                className="login-register-btn select-none flex whitespace-nowrap w-full max-w-sm font-medium rounded-md"
             >
                <Button onClick={() => router.push("/auth/login")} className="flex-1 rounded-none bg-transparent">
